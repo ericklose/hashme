749597b7115efa1b92ced8bbebbc5146439a4709
Eric Klose <ericklose@gmail.com>
2016-03-12 12:52:24 -0700

    starting login stuff by copying other project

--- Hash Me/DataService.swift
+++ Hash Me/DataService.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import Firebase
+
+let URL_BASE = "https://hashme.firebaseio.com"
+
+class DataService {
+    static let ds = DataService()
+    
+    private var _REF_BASE = Firebase(url: "\(URL_BASE)")
+    private var _REF_POST = Firebase(url: "\(URL_BASE)/posts")
+    private var _REF_USERS = Firebase(url: "\(URL_BASE)/users")
+    
+    var REF_BASE: Firebase {
+        return _REF_BASE
+    }
+    
+    var REF_POST: Firebase {
+        return _REF_POST
+    }
+    
+    var REF_USERS: Firebase {
+        return _REF_USERS
+    }
+    
+    var REF_USER_CURRENT: Firebase {
+        let uid = NSUserDefaults.standardUserDefaults().valueForKey(KEY_UID) as! String
+        let user = Firebase(url: "\(URL_BASE)").childByAppendingPath("users").childByAppendingPath(uid)
+        return user!
+    }
+    
+    func createFirebaseUser(uid: String, user: Dictionary<String, String>) {
+        REF_USERS.childByAppendingPath(uid).setValue(user)
+    }
+}
