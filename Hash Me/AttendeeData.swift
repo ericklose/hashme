0d6e8c84b9d277b97666d77ac3c797a0fc850a1b
Eric Klose <ericklose@gmail.com>
2016-03-14 12:03:38 -0600

    started building a trail attendee list

---
+++ Hash Me/AttendeeData.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  Hash Me
+//
+//  Created by Eric Klose on 3/14/16.
+//  Copyright © 2016 Eric Klose. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Attendee: Hasher {
+    
+    private var _paid_full: Bool!
+    
+    
+    var paid_full: Bool {
+        return _paid_full
+    }
+    
+    init(hasherHashName: hasherHashName) {
+        
+    }
+    
+    
+}
