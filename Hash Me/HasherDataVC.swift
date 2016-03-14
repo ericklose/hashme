c8aba4f92743371d674b50b464bbd3edc42b9179
Eric Klose <ericklose@gmail.com>
2016-03-14 14:13:31 -0600

    fixed storyboard and another file

--- Hash Me/HasherDataVC.swift
+++ Hash Me/HasherDataVC.swift
@@ -20,8 +20,6 @@ class HasherDataVC: UIViewController {
     @IBOutlet weak var kennelMembershipsTxtFld: UITextField!
     @IBOutlet weak var updateInfoBtn: UIButton!
     
- //   var hasher: Hasher!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -36,109 +34,32 @@ class HasherDataVC: UIViewController {
                 
                 let hashNames = hasherDict["hasherHashNames"] as? Dictionary<String, AnyObject>
                 
-                    self.hashNamesLbl.text = ""
-                    
-                    let keyArray = [String](hashNames!.keys)
-                    print(keyArray[0])
-                    self.hashNamesLbl.text = keyArray[0]
-<<<<<<< HEAD
+                self.hashNamesLbl.text = ""
                 
-=======
-                    
->>>>>>> 9a25ef9afbca23db1db3d1f76f21eb88d20a69ff
-                    if keyArray.count > 1 {
-                        for var x = 1; x < keyArray.count; x++ {
-                            let name = keyArray[x]
-                                print(name)
-                                self.hashNamesLbl.text! += ", \(name)"
-<<<<<<< HEAD
-=======
-                            
->>>>>>> 9a25ef9afbca23db1db3d1f76f21eb88d20a69ff
-                        }
+                let keyArray = [String](hashNames!.keys)
+                print(keyArray[0])
+                self.hashNamesLbl.text = keyArray[0]
+                
+                if keyArray.count > 1 {
+                    for var x = 1; x < keyArray.count; x++ {
+                        let name = keyArray[x]
+                        print(name)
+                        self.hashNamesLbl.text! += ", \(name)"
+                        
                     }
                 }
-                
-            //}
-            
-//            
-//            // You should check to see if a value exists for `orch_array` first.
-//            if let dict: AnyObject = NSUserDefaults().dictionaryForKey("orch_array")?[orchId] {
-//                // Then force downcast.
-//                let orch = dict as! [String: String]
-//                orch[appleId] // No error
-//            }
-//            2. Use optional binding to check if orch is nil:
-//            
-//            if let orch = NSUserDefaults().dictionaryForKey("orch_array")?[orchId] as? [String: String] {
-//                orch[appleId] // No error
-//            }
-//            
-            
-//            if let moves = dict["moves"] as? [Dictionary<String, AnyObject>]
-//                where moves.count > 0 {
-//                    if let name = moves[0]["name"] {
-//                        self._move = name.capitalizedString
-//                        
-//                    }
-//                    
-//                    if moves.count > 1 {
-//                        for var x = 1; x < moves.count; x++ {
-//                            if let name = moves[x]["name"] {
-//                                self._move! += ", \(name.capitalizedString)"
-//                            }
-//                        }
-//                    }
-//            } else {
-//                self._move = ""
-//            }
-
-            
-            
-            
-//            if let hasherDict2 = snapshot.value as? Dictionary<String, Array<String>> {
-//                
-//                
-//            }
+            }
             
         })
-       
-        
-        
-        
-        
-//        DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
-//            print(snapshot.value)
-//            
-//            self.posts = []
-//            
-//            if let snapshots = snapshot.children.allObjects as? [FDataSnapshot] {
-//                
-//                for snap in snapshots {
-//                    print("SNAP: \(snap)")
-//                    
-//                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
-//                        let key = snap.key
-//                        let post = Post(postKey: key, dictionary: postDict)
-//                        self.posts.append(post)
-//                    }
-//                }
-//            }
-//            
-//            self.tableView.reloadData()
-//        })
-
-        
-        
         
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func editInfoPressed(sender: AnyObject) {
         nerdNameTxtFld.hidden = false
         hashNamesTxtFld.hidden = false
@@ -153,6 +74,6 @@ class HasherDataVC: UIViewController {
     }
     
     
-
+    
 }
 
