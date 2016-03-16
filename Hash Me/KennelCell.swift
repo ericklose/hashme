b43248d60fa8bd22b1f787b6b8fcc8800240465f
Eric Klose <ericklose@gmail.com>
2016-03-16 15:57:04 -0600

    added some files for kennels

---
+++ Hash Me/KennelCell.swift
@@ -0,0 +1,24 @@
+//
+//  KennelCell.swift
+//  Hash Me
+//
+//  Created by Eric Klose on 3/16/16.
+//  Copyright © 2016 Eric Klose. All rights reserved.
+//
+
+import UIKit
+
+class KennelCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
