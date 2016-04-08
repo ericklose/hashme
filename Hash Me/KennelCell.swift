5d9abe6cd424578c8e8f0a72e024908661eeae53
Eric Klose <ericklose@gmail.com>
2016-04-08 19:56:58 -0400

    started kennel list

--- Hash Me/KennelCell.swift
+++ Hash Me/KennelCell.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class KennelCell: UITableViewCell {
+    
+    @IBOutlet weak var kennelName: UILabel!
+    @IBOutlet weak var kennelLocation: UILabel!
+    @IBOutlet weak var kennelSchedule: UILabel!
+    
+    var kennel: KennelData!
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -19,6 +25,14 @@ class KennelCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func configureCell(kennel: KennelData) {
+        self.kennel = kennel
+        
+        self.kennelName.text = kennel.kennelName
+        self.kennelLocation.text = kennel.kennelLocation
+        self.kennelSchedule.text = kennel.kennelSchedule
     }
 
 }
