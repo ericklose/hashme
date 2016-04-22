14a5359d2b6a61288d6fe4b1e242d20d844ccb17
Eric Klose <ericklose@gmail.com>
2016-04-22 10:07:09 -0400

    some progress on manage kennel

--- Hash Me/ManageKennelVC.swift
+++ Hash Me/ManageKennelVC.swift
@@ -14,48 +14,43 @@ class ManageKennelVC: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var kennelDetailsTableView: UITableView!
     
     @IBOutlet weak var kennelNameLbl: UILabel!
-    @IBOutlet weak var kennelNameEditLbl: UIButton!
-    @IBOutlet weak var kennelNameEditField: UITextField!
-    @IBOutlet weak var kennelNameSaveLbl: UIButton!
     @IBOutlet weak var kennelScheduleLbl: UILabel!
-    @IBOutlet weak var kennelScheduleEditLbl: UIButton!
-    @IBOutlet weak var kennelScheduleEditField: UITextField!
-    @IBOutlet weak var kennelScheduleSaveLbl: UIButton!
     @IBOutlet weak var kennelCountryLbl: UILabel!
-    @IBOutlet weak var kennelCountryEditLbl: UIButton!
-    @IBOutlet weak var kennelCountryEditField: UITextField!
-    @IBOutlet weak var kennelCountrySaveLbl: UIButton!
     @IBOutlet weak var kennelUsStateLbl: UILabel!
-    @IBOutlet weak var kennelUsStateEditLbl: UIButton!
-    @IBOutlet weak var kennelUsStateEditField: UITextField!
-    @IBOutlet weak var kennelUsStateSaveLbl: UIButton!
+    @IBOutlet weak var kennelEditBtn: UIButton!
     
     var kennels: KennelData!
+    var trails = [TrailData]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         kennelDetailsTableView.delegate = self
         kennelDetailsTableView.dataSource = self
+        //trailTableView.estimatedRowHeight = 200
+        
+        
+        DataService.ds.REF_TRAILS.observeEventType(.Value, withBlock: { snapshot in
+            
+            self.trails = []
+            
+            if let snapshots = snapshot.children.allObjects as? [FDataSnapshot] {
+                for snap in snapshots {
+                    if let trailDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let trail = TrailData(trailKey: key, dictionary: trailDict)
+                        self.trails.append(trail)
+                    }
+                }
+            }
+            self.kennelDetailsTableView.reloadData()
+        })
         
     }
     
     override func viewDidAppear(animated: Bool) {
-        kennelNameEditField.hidden = true
-        kennelNameSaveLbl.hidden = true
-        kennelScheduleEditField.hidden = true
-        kennelScheduleSaveLbl.hidden = true
-        kennelCountryEditField.hidden = true
-        kennelCountrySaveLbl.hidden = true
-        kennelUsStateEditField.hidden = true
-        kennelUsStateSaveLbl.hidden = true
     
         updateKennelDetails()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -63,97 +58,24 @@ class ManageKennelVC: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return trails.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCellWithIdentifier("kennelDetailsCell") as? KennelCell {
-            
-            //cell.configureCell(kennels)
+        let trail = trails[indexPath.row]
+        if let cell = tableView.dequeueReusableCellWithIdentifier("kennelTrailsCell") as? TrailCell {
+            cell.configureCell(trail)
             return cell
         } else {
-            return AttendeeCell()
+            return TrailCell()
         }
     }
     
     func updateKennelDetails() {
         kennelNameLbl.text = kennels.kennelName
-        kennelNameEditField.text = kennels.kennelName
         kennelScheduleLbl.text = kennels.kennelSchedule
-        kennelScheduleEditField.text = kennels.kennelSchedule
         kennelCountryLbl.text = kennels.kennelCountry
-        kennelCountryEditField.text = kennels.kennelCountry
         kennelUsStateLbl.text = kennels.kennelUsState
-        kennelUsStateEditField.text = kennels.kennelUsState
     }
-    
-    @IBAction func editKennelName(sender: UIButton) {
-        kennelNameEditField.hidden = false
-        kennelNameSaveLbl.hidden = false
-        kennelNameLbl.hidden = true
-        kennelNameEditLbl.hidden = true
-    }
-    
-    @IBAction func editKennelSchedule(sender: UIButton) {
-        kennelScheduleLbl.hidden = true
-        kennelScheduleEditLbl.hidden = true
-        kennelScheduleEditField.hidden = false
-        kennelScheduleSaveLbl.hidden = false
-    }
-    
-    @IBAction func editKennelCountry(sender: UIButton) {
-        kennelCountryLbl.hidden = true
-        kennelCountryEditLbl.hidden = true
-        kennelCountryEditField.hidden = false
-        kennelCountrySaveLbl.hidden = false
-    }
-    
-    @IBAction func editKennelUsState(sender: UIButton) {
-        kennelUsStateLbl.hidden = true
-        kennelUsStateEditLbl.hidden = true
-        kennelUsStateEditField.hidden = false
-        kennelUsStateSaveLbl.hidden = false
-    }
-    
-    @IBAction func saveKennelName(sender: UIButton) {
-        kennelNameLbl.hidden = false
-        kennelNameEditLbl.hidden = false
-        kennelNameEditField.hidden = true
-        kennelNameSaveLbl.hidden = true
-        if kennelNameEditField.text == nil || kennelNameEditField == "" {
-            return
-        } else {
-            postKennelToFirebase("name", saveValue: kennelNameEditField.text!)
-        }
-    }
-    
-    @IBAction func saveKennelSchedule(sender: UIButton) {
-        kennelScheduleLbl.hidden = false
-        kennelScheduleEditLbl.hidden = false
-        kennelScheduleEditField.hidden = true
-        kennelScheduleSaveLbl.hidden = true
-    }
-    
-    @IBAction func saveKennelCountry(sender: UIButton) {
-        kennelCountryLbl.hidden = false
-        kennelCountryEditLbl.hidden = false
-        kennelCountryEditField.hidden = true
-        kennelCountrySaveLbl.hidden = true
-    }
-    
-    @IBAction func saveKennelUsState(sender: UIButton) {
-        kennelUsStateLbl.hidden = false
-        kennelUsStateEditLbl.hidden = false
-        kennelUsStateEditField.hidden = true
-        kennelUsStateSaveLbl.hidden = true
-    }
-    
-    
-    func postKennelToFirebase(saveType: String, saveValue: String) {
-        
-        DataService.ds.REF_KENNELS.childByAppendingPath(kennels.kennelId).updateChildValues([saveType: saveValue])
-        
-    }
-    
     
 }
--- Hash Me/TrailCell.swift
+++ Hash Me/TrailCell.swift
@@ -7,8 +7,6 @@
 //
 
 import UIKit
-import Alamofire
-import Firebase
 
 class TrailCell: UITableViewCell {
     
