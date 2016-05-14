2047cc4e5900fc61067dc4182eb1a6e2ab59aab8
Eric Klose <ericklose@gmail.com>
2016-05-14 14:05:52 -0400

    added a data VC to have a playground

--- Hash Me/DataService.swift
+++ Hash Me/DataService.swift
@@ -23,7 +23,9 @@ class DataService {
     //private var _REF_HASHER_UID = NSUserDefaults.standardUserDefaults().valueForKey(KEY_UID) as? String
     //note that the ClaimHasherIdVC now uses REF_UID instead of originally using REF_HASHER_UID so that's a change to undo too if needed
     //NEW
+    //REF_HASHER_UID is the ID for the hasher owned by the user. The naming is bad but this was the least destructive way to change it.
     private var _REF_HASHER_UID: String!
+    //REF_UID is the user ID which is logged in. Since it isn't a hasher ID, it really shouldn't be used (unless we want to give Eric & Holly global admin or something).
     private var _REF_UID = NSUserDefaults.standardUserDefaults().valueForKey(KEY_UID) as? String
     
     
@@ -52,7 +54,7 @@ class DataService {
     //        return _REF_HASHER_UID!
     //    }
     
-    //MIGRATE TO THIS
+    //NEW SYSTEM
     func updateRefHasherUserId(hasherId: String) {
         _REF_HASHER_UID = hasherId
     }
@@ -60,8 +62,9 @@ class DataService {
     var REF_HASHER_USERID: String {
             return _REF_HASHER_UID
     }
-    //END MIGRATION CHANGES
+    //END OF NEW SYSTEM CHANGES
     
+    //SHIT, THIS PROBABLY NEEDS TO BE DISABLED
     var REF_HASHER_CURRENT: Firebase {
         let uid = NSUserDefaults.standardUserDefaults().valueForKey(KEY_UID) as! String
         let hasher = Firebase(url: "\(URL_BASE)").childByAppendingPath("hashers").childByAppendingPath(uid)
