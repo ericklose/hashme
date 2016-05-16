b4d6b4437c5773896c86cd59b91f6634805cab12
Eric Klose <ericklose@gmail.com>
2016-05-16 16:47:32 -0400

    finished claim system, toggled to production

--- Hash Me/TrailListVC.swift
+++ Hash Me/TrailListVC.swift
@@ -22,14 +22,7 @@ class TrailListVC: UIViewController, UITableViewDelegate, UITableViewDataSource 
         trailTableView.delegate = self
         trailTableView.dataSource = self
         trailTableView.estimatedRowHeight = 150
-
-        print("BACK : ", presentingViewController)
         
-//        if presentingViewController
-            //== ClaimHashIdVC {
-        self.navigationItem.hidesBackButton = true
-//        }
-
         //CREATE HASHER ID TO HASHER DICT AND PASS TO TRAIL CELL (VIA CONFIGURE CELL)
         
         DataService.ds.REF_TRAILS.observeEventType(.Value, withBlock: { snapshot in
